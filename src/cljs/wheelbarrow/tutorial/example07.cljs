2f0ed4872641e37e6637e409d0e70f73486f63ec
Tom White <tom@sixdozen.com>
2013-08-13 01:00:21 -0700

    removed unnecessary require in example

--- src/cljs/wheelbarrow/tutorial/example07.cljs
+++ src/cljs/wheelbarrow/tutorial/example07.cljs
@@ -1,6 +1,5 @@
 (ns wheelbarrow.tutorial.example07
-  (:require [cljs.reader :refer [read-string]]
-            [wheelbarrow :refer [Rickshaw bootstrap]]))
+  (:require [wheelbarrow :refer [Rickshaw bootstrap]]))
 
 (bootstrap)
 
